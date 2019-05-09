8a41d0969741fe989f501b1894f68fe83f4a3e66
William <william@fontrip.com>
2019-05-09 23:15:28 +0800

    修改 AppServiceProvider 檔

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Work;
 use App\WorkTag;
 use Jenssegers\Agent\Agent;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,16 +25,22 @@ class AppServiceProvider extends ServiceProvider
             'agent' => $agent,
         ]);
 
-        $distinct_works = Work::select('id', 'title', 'date')->orderBy('date', 'desc')->get();
+        $distinct_works = Schema::hasTable('works')
+            ? Work::select('id', 'title', 'date')->orderBy('date', 'desc')->get()
+            : [];
         view()->share('distinct_works', $distinct_works);
 
         $featured_tags = ["Laravel", ];
         view()->share('featured_tags', $featured_tags);
 
-        $distinct_ordinary_tags = WorkTag::distinct()->whereNotNull('name')->where('type', '一般')->orderBy('name', 'desc')->pluck('name')->all();
+        $distinct_ordinary_tags = Schema::hasTable('work_tags')
+            ? WorkTag::distinct()->whereNotNull('name')->where('type', '一般')->orderBy('name', 'desc')->pluck('name')->all()
+            : [];
         view()->share('distinct_ordinary_tags', $distinct_ordinary_tags);
 
-        $distinct_year_tags = WorkTag::distinct()->whereNotNull('name')->where('type', '年分')->orderBy('name', 'desc')->pluck('name')->all();
+        $distinct_year_tags = Schema::hasTable('work_tags')
+            ? WorkTag::distinct()->whereNotNull('name')->where('type', '年分')->orderBy('name', 'desc')->pluck('name')->all()
+            : [];
         view()->share('distinct_year_tags', $distinct_year_tags);
     }
 
