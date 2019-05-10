f4ffe1307c5a49f31a6abf4f75f84361ab4e1eaf
William <william@fontrip.com>
2019-05-10 11:05:39 +0800

    交換麵包屑和標記雲位置

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,9 +1,5 @@
 <div class="d-flex flex-wrap my-2">
     <div class="col-md-12 mb-2">
-        {{ Breadcrumbs::render() }}
-    </div>
-
-    <div class="col-md-12 mb-2">
         @foreach ($distinct_ordinary_tags as $distinct_ordinary_tag)
             @if (in_array($distinct_ordinary_tag, $featured_tags))
                 <a href="{{ route('works.index', ['q' => $distinct_ordinary_tag,]) }}" class="btn btn-sm btn-outline-danger btn-tag {{ $request->q == $distinct_ordinary_tag ? 'active' : '' }}">
@@ -16,5 +12,9 @@
                 </a>
             @endif
         @endforeach
+    </div>
+
+    <div class="col-md-12 mb-2">
+        {{ Breadcrumbs::render() }}
     </div>
 </div>
