9fee88a28c820e5fc166fa7615eaf2c8a6456ec5
Memo Chou <memochou1993@gmail.com>
2020-10-28 23:57:14 +0800

    Fix date in navbar

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,7 +1,7 @@
 
 <div class="list-group">
     @foreach ($distinct_works as $distinct_work)
-        <a href="{{ route('works.show', $distinct_work->id) }}" class="list-group-item list-group-item-action {{ (Request::is('works/'.$distinct_work->id) or Request::is('works/'.$distinct_work->id.'/edit') or Request::is('works/'.$distinct_work->id.'/workTags/create')) ? 'active' : '' }}" @if (!$agent->isMobile()) data-toggle="tooltip" @endif title="{{ $distinct_work->date }}">
+        <a href="{{ route('works.show', $distinct_work->id) }}" class="list-group-item list-group-item-action {{ (Request::is('works/'.$distinct_work->id) or Request::is('works/'.$distinct_work->id.'/edit') or Request::is('works/'.$distinct_work->id.'/workTags/create')) ? 'active' : '' }}" @if (!$agent->isMobile()) data-toggle="tooltip" @endif title="{{ $distinct_work->begin_date == $distinct_work->end_date ? $distinct_work->begin_date : $distinct_work->begin_date . ' - ' . $distinct_work->end_date }}">
             <div class="first-line-outdent">
                 {{ $distinct_work->title }}
             </div>
