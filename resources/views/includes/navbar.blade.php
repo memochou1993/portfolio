936cdd0708b7f6a3e4534c2b5cfdbb1a46915b66
Memo Chou <38420282+memochou1993@users.noreply.github.com>
2021-01-18 10:37:58 +0800

    Update navbar.blade.php

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 	<a class="navbar-brand" href="{{ route('home.index') }}">
-		周孝威作品集@if (!$agent->isMobile())（2012 - 2020）@endif
+		周孝威作品集@if (!$agent->isMobile())（2012 - {{ date("Y") }}）@endif
 	</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
