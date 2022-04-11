ed0d7eb3a7457150a33eb860be3c94c0e151061a
Memo Chou <memochou1993@gmail.com>
2022-04-11 22:54:55 +0800

    Update title

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -2,7 +2,7 @@
 	<a class="navbar-brand" href="{{ route('home.index') }}">
 		<span>Memo's Portfolio</span>
 		@if (!$agent->isMobile())
-			<span>（2012 - {{ date("Y") }}）</span>
+			<span>(2012 - {{ date("Y") }})</span>
 		@endif
 	</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
