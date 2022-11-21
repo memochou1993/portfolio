98d2e779e6a0a486b3026ef26d3a59f986b60994
Memo Chou <memochou1993@gmail.com>
2022-11-22 00:29:47 +0800

    Add meta tags

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -3,6 +3,14 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>Memo's Portfolio</title>
+<meta name="author" content="Memo Chou" />
+<meta name="description" content="Memo's Portfolio" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Memo's Portfolio" />
+<meta property="og:description" content="Memo's Portfolio" />
+<meta property="og:image" content="https://github.com/memochou1993.png" />
+<meta property="og:image:alt" content="Memo Chou" />
+<meta property="og:image:type" content="image/png" />
 <link href="{{ asset('images/favicon.png') }}" rel="shortcut icon">
 <link href="{{ asset('css/loading.css') }}" rel="stylesheet">
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
